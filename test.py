2499b75876be070631f5780ae5380bab5f0bbb37
Alin <alin.clincea@gmail.com>
2019-06-10 23:28:44 +0300

    * now it stops when it detects a response containing "Error"

--- test.py
+++ test.py
@@ -1,27 +1,27 @@
-import requests, re
-
-number = ''
-year = ''
+import requests, re, sys
 
 #check the input and continue only if it is valid
-def issueInput():
-	global number
-	global year
+while True:
 	issue = input("Scrie numărul și anul Monitorului Oficial (număr/an): ")
-	pattern = r'([\dbis]*?)/(\d{4})'
-	result = re.match(pattern, issue)
-	while (result == None): #test a regex pattern [\dbis]*?/\d{4}
+	pattern = r'([\d bis]*?)/(\d{4})'
+	result = re.match(pattern, issue, re.IGNORECASE)
+	if result == None: #test a regex pattern [\dbis]*?/\d{4}
 		print("Numărul sau anul nu sunt scrise corect. Mai încearcă o dată.")
-		issueInput()
-	#if the issue is valid separate number from year and return the two values as a list [number, year]
-	number = result.groups()[0].zfill(4)
-	year = result.groups()[1]
-	return [number, year]
+		continue
+	else:
+		#if the issue is valid separate number from year and return the two values
+		#check if bis is present
+		number = result.groups()[0].replace(' ', '').lower()
+		if number.find('bis') >= 0:
+			number = number.replace('b', 'B').zfill(7)
+		else:
+			number = number.zfill(4)
+		year = result.groups()[1]
+		break
 
-issue = issueInput()
-url = 'http://www.monitoruloficial.ro/emonitornew/services/view.php' #doc=0120190435&format=json&page=1
+url = 'http://www.monitoruloficial.ro/emonitornew/services/view.php'
 user_agent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'
-referer = 'http://www.monitoruloficial.ro/emonitornew/emonviewmof.php' #?fid=MS43OTEzNzc4NjUyNTc1RSszMA==
+referer = 'http://www.monitoruloficial.ro/emonitornew/emonviewmof.php'
 			
 headers = {'User-Agent': user_agent,
 			'Referer': referer,
@@ -33,13 +33,16 @@ params = {'doc': '01' + year + number,
 			
 session = requests.Session()
 
-for i in range(1, 5):
+for i in range(1, 1000):
 	params['page'] = str(i)
 	response = session.get(url, headers = headers, params = params)
+	if str(response.content).find('Error') >= 0:
+		break
 	file_location = 'C:/Users/Alin/Desktop/'
 	file_name = file_location + number + '-' + params['page'] + '.jpg'
 	with open(file_name, 'wb') as fd:
 		for chunk in response.iter_content(chunk_size=128):
 			fd.write(chunk)
-
-print('Done!')
+	print(str(i), end=' ')
+	sys.stdout.flush()
+print('Gata!')
