78348471f899a358a584b357468eb8a07492caae
Alin <alin.clincea@gmail.com>
2019-06-12 19:00:05 +0300

    * minor edits

--- mo.py
+++ mo.py
@@ -1,16 +1,18 @@
 manifest = """mo.py
 0.0.6
 *********************************************************************************************
-Script pentru generarea de documente .pdf pe baza imaginilor publicate de M.O.
+Script pentru generarea de documente .pdf, în Windows, pe baza imaginilor publicate de M.O.
 Documentul .pdf va fi salvat pe Desktop.
 
 Funcționează numai pentru numerele publicate din 06.06.2017 (de la Partea I, nr. 414/2017)
 până în prezent.
 
 Formatul de input este:
-[parte/]număr/an, indicarea părții este opțională doar dacă se caută un număr din Partea I
+	[parte/]număr/an
 
-Pentru Partea I în limba maghiară, trebuie folosit 1m (ex. 1m/21/2019).
+Note:
+* indicarea părții este opțională doar dacă se caută un număr din Partea I
+* pentru Partea I în limba maghiară, trebuie folosit 1m (ex. 1m/21/2019).
 
 Exemple de utilizare:
 1. 1/414/2017 echivalent cu 414/2017 => Partea I, nr. 414 din 2017
